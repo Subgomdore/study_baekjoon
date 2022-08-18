64983329c6f80a24ccf8b05bb0dd4139e0f9ea31
Subgomdore <106915150+Subgomdore@users.noreply.github.com>
2022-08-18 12:04:37 +0900

    Main문제풀이 후 제출. Lv4추가

--- src/step03_loop/Lv4_Receipt.java
+++ src/step03_loop/Lv4_Receipt.java
@@ -1,7 +1,20 @@
+package step03_loop;
 
 import java.util.Scanner;
 
-public class Main {
+//BaekJoon 문제 25304번 
+//영수증에 적힌, 구매한 각 물건의 가격과 개수, 구매한 물건들의 총 금액을 보고
+//구매한 물건의 가격과 개수로 계산한 총 금액이 영수증에 적힌 총 금액과 일치하는지 검사해보자.
+
+//예제 입력1 		예제 출력1
+//260000		Yes (No)
+//4
+//20000 5
+//30000 2
+//10000 6
+//5000 8			
+
+public class Lv4_Receipt {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 
@@ -14,9 +27,10 @@ public class Main {
 				array[i][k] = value;
 			}
 		}
-		
+
 		int sum = 0;
 		int lastsum = 0;
+
 		for (int i = 0; i < tc; i++) {
 			sum = array[i][0] * array[i][1];
 			lastsum += sum;
